3e4bea40b44640c0314e7d7c36cf352163a40927
MinJeong <106390284+Mineong@users.noreply.github.com>
2025-04-23 14:54:30 +0900

    4교시

--- src/workshop/person/entity/personEntity.java
+++ src/workshop/person/entity/personEntity.java
@@ -6,9 +6,9 @@ public class personEntity {
 	private String ssn;
 	private String address;
 	private String phone;
-	
+
 	public personEntity() {
-		
+
 	}
 
 	public personEntity(String name, String ssn, String address, String phone) {
@@ -18,5 +18,51 @@ public class personEntity {
 		this.address = address;
 		this.phone = phone;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+		char genderNum = ssn.charAt(6);
+		if (genderNum == '1' || genderNum == '3') {
+//			this.gender = '³²';
+			setGender('³²');
+		} else {
+			setGender('¿©');
+		}
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
